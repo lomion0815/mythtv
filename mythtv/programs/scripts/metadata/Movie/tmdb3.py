94e555877ada476179a384b31d7377f6b13cddbf
Lomion0815 <lomion@aon.at>
2017-06-16 14:27:40 +0200

    Metadatalookup: Use year in filename for lookup

--- mythtv/programs/scripts/metadata/Movie/tmdb3.py
+++ mythtv/programs/scripts/metadata/Movie/tmdb3.py
@@ -29,6 +29,7 @@ __version__ = "0.3.7"
 
 from optparse import OptionParser
 import sys
+import re
 
 def buildSingle(inetref, opts):
     from MythTV.tmdb3.tmdb_exceptions import TMDBRequestInvalid
@@ -123,10 +124,18 @@ def buildList(query, opts):
     # as negative to all text that comes afterwards
     query = query.replace('-',' ')
 
+    query = query.strip() # trim extra whitespace
+    yearRegex = re.compile(r"\(?((19|20)[0-9]{2})\)?$")
+    year = yearRegex.search(query)
+    if year is not None:
+        year = year.group(1)
+    query = yearRegex.sub("", query)
+    query = query.strip()
+
     from MythTV.tmdb3 import searchMovie
     from MythTV import VideoMetadata
     from lxml import etree
-    results = iter(searchMovie(query))
+    results = iter(searchMovie(query, locale=None, adult=False, year=year))
     tree = etree.XML(u'<metadata></metadata>')
     mapping = [['runtime',      'runtime'],     ['title',       'originaltitle'],
                ['releasedate',  'releasedate'], ['tagline',     'tagline'],
